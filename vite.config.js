11b8fca40733abca34171834b6a77ef8874894d6
3Melody <plugeiei2003@gmail.com>
2024-08-18 17:30:37 +0700

    firebase

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
       external: ['firebase/firestore'],
+    },
+  },
+  resolve: {
+    alias: {
+      'firebase/firestore': resolve(__dirname, 'node_modules/firebase/firestore'),
     },
   },
   plugins: [react()],
